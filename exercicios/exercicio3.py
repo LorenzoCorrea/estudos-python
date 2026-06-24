d34f028cd30c5c7c749e3692e07abaaf05b25e83
LorenzoCorrea <lorenzo.correa002@gmail.com>
2026-06-24 11:14:32 -0300

    Adicionei um arquivo de explicação de elif e o exercicio 3

---
+++ exercicios/exercicio3.py
@@ -0,0 +1,23 @@
+'''
+
+Exercicio 1:
+
+Calculo de distancia:
+-> Escreva um programa que pergunte a distância que um passageiro
+  deseja percorrer em km
+
+  Calcule o preço da passagem, cobrando R$ 0,50 por km para viagens de até 200km
+  E R$0,35 para viagens mais longas.
+
+'''
+
+'''
+
+Exercicio 2:
+
+Aumento de salário do funcionário:
+-> Escreva um programa que pergunte o salário de um funcionario
+e calcule o valor do aumento.
+Para salários superiores a R$ 1.250,00 calcule um aumento de 10%
+Para inferiores ou iguais, de 15%.
+'''
