5c4e76deb12a37f381d04e9c2fd8fdc753dc10b2
LorenzoCorrea <lorenzo.correa002@gmail.com>
2026-06-24 11:43:33 -0300

    Adiciona os exercicios feitos de if else

--- exercicios/exercicio3.py
+++ exercicios/exercicio3.py
@@ -11,13 +11,30 @@ Calculo de distancia:
 
 '''
 
-'''
+  # distancia = float(input("Qual a distancia que deseja percorrer: \n"))
+  # if distancia <= 200:
+  #     preco = distancia * 0.50
+  # else: 
+  #    preco = distancia * 0.35
 
-Exercicio 2:
+  # print(f"O valor da passagem é de R${preco}")
 
-Aumento de salário do funcionário:
--> Escreva um programa que pergunte o salário de um funcionario
-e calcule o valor do aumento.
-Para salários superiores a R$ 1.250,00 calcule um aumento de 10%
-Para inferiores ou iguais, de 15%.
-'''
+
+
+
+# Exercicio 2:
+# Aumento de salário do funcionário:
+# -> Escreva um programa que pergunte o salário de um funcionario
+# e calcule o valor do aumento.
+# Para salários superiores a R$ 1.250,00 calcule um aumento de 10%
+# Para inferiores ou iguais, de 15%
+
+
+salario = float(input("Digite seu salário: \n"))
+
+if salario > 1250.00: 
+  aumento = salario * 0.10
+else:
+   aumento = salario * 0.15
+
+print(f"O valor do aumento é de R${aumento}")
