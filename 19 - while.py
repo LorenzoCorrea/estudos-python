68fc3532884a9b4673810907c1f308708753cc1e
LorenzoCorrea <lorenzo.correa002@gmail.com>
2026-06-24 16:52:34 -0300

    add archive anotações.txt

--- 19 - while.py
+++ 19 - while.py
@@ -7,8 +7,8 @@ average = 0
 while(rating != -1):
   rating = float(input("Informe a nota do jogo: \n"))
   if(rating != -1):
-    totalRating += rating 
-    qtdRating += 1
-    average = totalRating / qtdRating
-
+    totalRating += rating  ###Pegue o total atual e some com a nova nota
+    qtdRating += 1 ###Adiciona 1 à contagem de pessoas que votaram.
+    average = totalRating / qtdRating ###Calcula a média.
+ 
 print(f"A média das avaliações do jogo {gameName} é {average:.2f}")
