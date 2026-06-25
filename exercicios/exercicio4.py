8a2ec139051dbb675be58355d593302209595eed
LorenzoCorrea <lorenzo.correa002@gmail.com>
2026-06-25 10:50:41 -0300

    Resolução dos exercicios

--- exercicios/exercicio4.py
+++ exercicios/exercicio4.py
@@ -15,6 +15,10 @@ print("... Boom!!")
 # Faça um programa que calcule a tabuada de um número, com
 #  valores iniciais e finais informados pelo usuário
 
-num = int(input("Digite o numero: \n"))
-for num in range(10):
-    print(f"A tabuada do {num} é:")
+number = int(input("Tabuada de: "))
+begin = int(input("De: "))
+end = int(input("Até: "))
+x = begin
+while x <= end:
+    print(f"{number} x {x} = {number * x}")
+    x = x + 1
