01d249914e7bb86b23b6b5b2430eecbeab3356b5
LorenzoCorrea <lorenzo.correa002@gmail.com>
2026-06-29 10:52:23 -0300

    os + exercicio

---
+++ Modulos/3 - os.py
@@ -0,0 +1,24 @@
+import os
+
+
+# 1 - consultar as funcionalidades do módulo
+#help('os')
+
+
+# 2 - Retornar a pasta atual
+print(os.getcwd())
+
+# 3 - listar arquivos e pastas
+print(os.listdir())
+
+# 4 - Apresentar a versão da OS
+os.system("ver")
+
+# 5 - configurações da maquina
+os.system("systeminfo")
+
+# 6 - limpar a tela
+os.system("cls")
+
+# 7 - verificar o ip
+os.system("ipconfig")
