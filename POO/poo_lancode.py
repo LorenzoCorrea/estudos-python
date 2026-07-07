44a325fb861004a64a00dfbce69a6e9bbd440b85
LorenzoCorrea <lorenzo.correa002@gmail.com>
2026-07-07 16:36:00 -0300

    encapsulamento

--- POO/poo_lancode.py
+++ POO/poo_lancode.py
@@ -22,9 +22,23 @@ class Canal:
 
 class CanalEmpresarial(Canal): ##O canal empresarial herdou do "CANAL"
   def __init__(self, nome, descricao, inscritos):
-    super().__init__(nome, descricao, inscritos)
+    super().__init__(nome, descricao, inscritos) ###Super chama a classe pai, de cima ou seja a CANAL
+    self.equipe = []
 
 canal_lohn= Canal("Lohn", "Sem desc", 0)
 canal_lancode = Canal("Lancode", "Códigos e Gatos", 65600)
+canal_duolingo = CanalEmpresarial("Duolingo", "ingres", 6000000)
 
+
+print(canal_duolingo.descricao)
+canal_duolingo.equipe.append("Ana")
+canal_duolingo.equipe.append("Lorenzo")
+canal_duolingo.equipe.append("Pedro")
+canal_duolingo.equipe.append("Ana")
+
+
+
+
+
+print(canal_duolingo.equipe)
 
