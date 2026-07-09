760dd928237686b25f7fe8cb0d33c7a559b9e6d9
Lorenzo Correa <lorenzo.correa002@gmail.com>
2026-07-09 11:31:20 -0300

    exercicio 5

--- Curso-Python-Gustavo-Guanabara-master/mundo_1/Aula_6_Primeiros_Passos/divertimento_1.py
+++ Curso-Python-Gustavo-Guanabara-master/mundo_1/Aula_6_Primeiros_Passos/divertimento_1.py
@@ -1,6 +1,4 @@
 # Tipos primitivos: int()
 
-n1 = int(input("Digite um valor: \n"))
-n2 = int(input("Digite outro valor :\n"))
-s = n1 + n2
-print("A soma entre {} e {} vale {}.".format(n1, n2, s))
+n = int(input("Digite um numero: "))
+print(f"O tipo primitivo desse valor é: {type(n)}")
