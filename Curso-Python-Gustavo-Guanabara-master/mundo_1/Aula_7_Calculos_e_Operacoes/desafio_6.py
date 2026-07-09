2c6fe5d9b4566258918a2473232d9abb0b8ba060
Lorenzo Correa <lorenzo.correa002@gmail.com>
2026-07-09 14:08:39 -0300

    desafio 6

--- Curso-Python-Gustavo-Guanabara-master/mundo_1/Aula_7_Calculos_e_Operacoes/desafio_6.py
+++ Curso-Python-Gustavo-Guanabara-master/mundo_1/Aula_7_Calculos_e_Operacoes/desafio_6.py
@@ -1,6 +1,8 @@
 # Crie um algoritmo que leia um número e mostre o seu dobro, o seu triplo e sua raiz quadrada:
-
-n = int(input("Digite um número: \n"))
-print("O dobro de {} é: {}!".format(n, n * 2))
-print("O triplo de {} é: {}.".format(n, n * 3))
-print("A raiz quadrada de {} é: {}.".format(n, n ** (1 / 2)))
+n = int(input("Digite um numero: "))
+dobro = n * 2
+tri = n * 3
+rq = n ** 0,5
+print(f"O dobro de {n} é: {dobro}")
+print(f"O triplo de {n} é: {tri}")
+print(f"A raiz quadrada de {n} é: {rq}")
