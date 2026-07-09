1a3c44c0e235ec4dee98372666993dc9a7aa3891
LorenzoCorrea <lorenzo.correa002@gmail.com>
2026-07-09 10:44:21 -0300

    exercicio

--- Curso-Python-Gustavo-Guanabara-master/mundo_1/Aula_1_a_5_Conhecendo_a_Linguagem/desafio_1.py
+++ Curso-Python-Gustavo-Guanabara-master/mundo_1/Aula_1_a_5_Conhecendo_a_Linguagem/desafio_1.py
@@ -1,10 +1,8 @@
 # Crie um script Python que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas de acordo com o valor digitado
 
-entrada = input("Digite seu nome: \n")
-print("Olá, {}! Prazer em conhecer você!".format(entrada))
+nome = str(input("Digite seu nome: "))
+print(f"Seja bem vindo: {nome}")
 
 # Crie um programa que escreva "Olá, mundo" na tela.
+print("Olá, mundo")
 
-print("Olá, mundo!")
-msg = "Hello, world!"
-print(msg)
