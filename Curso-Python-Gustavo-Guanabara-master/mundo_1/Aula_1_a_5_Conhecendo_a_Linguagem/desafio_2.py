09e5815831ee9c2f7ebc81e37127ab3361ff1ab4
Lorenzo Correa <lorenzo.correa002@gmail.com>
2026-07-09 11:07:49 -0300

    desafio 3

--- Curso-Python-Gustavo-Guanabara-master/mundo_1/Aula_1_a_5_Conhecendo_a_Linguagem/desafio_2.py
+++ Curso-Python-Gustavo-Guanabara-master/mundo_1/Aula_1_a_5_Conhecendo_a_Linguagem/desafio_2.py
@@ -2,4 +2,8 @@
 o mês e o ano de nascimento de uma pessoa 
 e mostre uma mensagem com a data formatada
 '''
-print
+dia = int(input("Digite o dia do seu nascimento: \n"))
+mes = str(input("Digite o mes do seu nascimento: \n"))
+ano = int(input("Digite o ano do seu nascimento: \n"))
+
+print(f"Sua data de nascimento é: {dia} de {mes} de {ano}")
