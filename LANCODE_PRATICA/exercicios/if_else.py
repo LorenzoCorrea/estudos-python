6190e3b775c12b6b82829c486e39c0e880331f2d
Lorenzo Correa <163217886+LorenzoCorrea@users.noreply.github.com>
2026-08-11 17:10:50 -0300

    exercicios finalizados

--- LANCODE_PRATICA/exercicios/if_else.py
+++ LANCODE_PRATICA/exercicios/if_else.py
@@ -6,15 +6,31 @@
 # Verificador de Nota Mínima: Crie um código que pergunte ao usuário qual foi sua nota em um teste. Defina uma nota mínima para aprovação (por exemplo, 6). 
 # Use uma estrutura if para verificar se a nota do usuário é maior ou igual à nota mínima. Se for, exiba a mensagem "Você atingiu a nota mínima!".
 
-notaMinima = float(input("Digite a sua nota: \n"))
-if notaMinima > 6:
-  print("Você foi aprovado!\n")
+# notaMinima = float(input("Digite a sua nota: \n"))
+# if notaMinima > 6:
+#   print("Você foi aprovado!\n")
+# else:
+#   print("Tente novamente outra hora :( \n")
+
+# Verificação de Diferença: Crie um código que peça para o usuário inserir dois nomes. 
+# Depois verifique se os dois nomes são diferentes. Se forem, exiba "Os nomes digitados são diferentes."
+# nome1 = input("Insira o primeiro nome: \n")
+# nome2 = input("Insira o segundo nome: \n")
+
+# if nome1 == nome2:
+#   print("Os dois nomes são iguais!")
+# else:
+#   print("Os dois nomes são diferentes")
+
+
+
+# Elegibilidade para um Evento (Idade Mínima): Imagine um evento para maiores de 15 anos. Crie um código que pergunte a idade do usuário. 
+# Verifique se a idade do usuário é maior ou igual a 15. Se for, exiba "Você pode participar do evento!".
+idade= int("Digite sua idade: \n")
+if idade >= 15:
+  print("Você pode participar do evento!")
 else:
-  print("Tente novamente outra hora :( \n")
-
-# Verificação de Diferença: Crie um código que peça para o usuário inserir dois nomes. Depois verifique se os dois nomes são diferentes. Se forem, exiba "Os nomes digitados são diferentes.".
-
-# Elegibilidade para um Evento (Idade Mínima): Imagine um evento para maiores de 15 anos. Crie um código que pergunte a idade do usuário. Verifique se a idade do usuário é maior ou igual a 15. Se for, exiba "Você pode participar do evento!".
+  print("Você não pode entrar")
 
 # Lembre-se que o objetivo é praticar o uso do if e dos operadores de comparação que vimos nesta aula. Não se preocupe se encontrar dificuldades, faz parte do aprendizado! Na próxima aula, resolveremos juntos esses exercícios. 😉
 
